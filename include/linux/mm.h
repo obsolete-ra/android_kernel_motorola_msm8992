2a48d8f60a197dd5b0fc5f45e90702423d7cbca0
xyyx <xyyx@mail.ru>
2017-03-06 19:41:47 +0800

    Increase max readahead value to 1024
    Conflicts:
    	include/linux/mm.h

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1636,7 +1636,7 @@ void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
 
-#define VM_MAX_READAHEAD	512	/* kbytes */
+#define VM_MAX_READAHEAD	1024	/* kbytes */
 #define VM_MIN_READAHEAD	32	/* kbytes (includes current page) */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
