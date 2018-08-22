c58d4ee0069a012140cb7a7e0fabf36b9cb9d15c
Francisco Franco <franciscofranco.1990@gmail.com>
2018-08-22 17:16:27 +0000

    power: wl: timerfd is never acquired
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,8 +16,8 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT				"IPA_WS;wlan_wow_wl;wlan_extscan_wl;qcom_rx_wakelock;wlan;bluedroid_timer;[timerfd];netmgr_wl;NETLINK"
+#define LIST_WL_DEFAULT				"IPA_WS;wlan_wow_wl;wlan_extscan_wl;qcom_rx_wakelock;wlan;bluedroid_timer;netmgr_wl;NETLINK"
 
 #define LENGTH_LIST_WL				255
-#define LENGTH_LIST_WL_DEFAULT		101
+#define LENGTH_LIST_WL_DEFAULT		91
 #define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
